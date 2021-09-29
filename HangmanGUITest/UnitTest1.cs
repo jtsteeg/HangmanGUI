9d25a7d16a9d741232277af98d09111ad7a5d0ef
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-09-29 18:38:29 -0500

    adds test project and updates project references

---
+++ HangmanGUITest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//include classes to test here
+
+namespace HangmanGUITest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
