98ed80b2fafcc8dae6500f24ef50ad7256166a49
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-10-03 17:34:04 -0500

    adds wordLibrary class and method to print unguessed words to GUI

--- HangmanGUI/Form1.cs
+++ HangmanGUI/Form1.cs
@@ -12,10 +12,16 @@ namespace HangmanGUI
 {
     public partial class HangmanGUI : Form
     {
+
+        List<Label> letterDisplay = new List<Label>();
         public HangmanGUI()
         {
             InitializeComponent();
+            
+            drawLines();
         }
+
+
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -35,6 +41,23 @@ namespace HangmanGUI
         private void LivesLeftLabel_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void drawLines()
+        {
+            WordLibrary WL = new WordLibrary();
+            string mysteryWord = WL.GetRandomWord();
+            char[] mysteryList = mysteryWord.ToCharArray();
+            int spacing = 800 / mysteryList.Length;
+            for (int i = 0; i < mysteryList.Length; i++)
+            {
+                letterDisplay.Add(new Label());
+                letterDisplay[i].Location = new Point((i * spacing) + 10, 80);
+                letterDisplay[i].Text = "*";
+                letterDisplay[i].Parent = MysteryWordBox;
+                letterDisplay[i].BringToFront();
+                letterDisplay[i].CreateControl();
+            }
         }
     }
 }
---
+++ HangmanGUI/WordLibrary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanGUI
+{
+    class WordLibrary
+    {
+        public List<string> wordSet { get; set; }
+        public WordLibrary()
+        {
+            wordSet = new List<string>() { "horse", "chicken", "llama", "cow", "duck", };
+        }
+
+        public string GetRandomWord()
+        {
+            var random = new Random();
+            var i = random.Next(0, wordSet.Count - 1);
+            return wordSet[i];
+        }
+    }
+}
