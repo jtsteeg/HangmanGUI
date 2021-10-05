138c03469bf9a3943a2e86b9158e5c9b5c369b24
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-10-05 17:06:56 -0500

    adds tests for characterGuess class

--- HangmanGUITest/CharacterGuess_tests.cs
+++ HangmanGUITest/CharacterGuess_tests.cs
@@ -8,11 +8,56 @@ namespace HangmanGUITest
     public class CharacterGuess_tests
     {
         [TestMethod]
-        public void Logic_validInput_returns2()
+        public void Logic_validCorrectInput_returns2()
         {
             var guessInput = 'c';
             var wordBeingGuessed = "cat";
             var expected = 2;
+
+            var sut = new CharacterGuess();
+
+            int result = sut.logic(guessInput, wordBeingGuessed);
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod]
+        public void Logic_validIncorrectInput_returns3()
+        {
+            var guessInput = 'x';
+            var wordBeingGuessed = "cat";
+            var expected = 3;
+
+            var sut = new CharacterGuess();
+
+            int result = sut.logic(guessInput, wordBeingGuessed);
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod]
+        public void Logic_invalidInput_returns1()
+        {
+            var guessInput = '2';
+            var wordBeingGuessed = "cat";
+            var expected = 1;
+
+            var sut = new CharacterGuess();
+
+            int result = sut.logic(guessInput, wordBeingGuessed);
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod]
+        public void Logic_noInput_returns1()
+        {
+            var guessInput = ' ';
+            var wordBeingGuessed = "cat";
+            var expected = 1;
 
             var sut = new CharacterGuess();
 
